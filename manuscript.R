6e6e2dc78272fc60b7b999a09765a5c5c2bdf962
Zhihao Ding <zhihao.ding@gmail.com>
2018-02-08 17:31:59 +0000

    updated scripts

--- manuscript.R
+++ manuscript.R
@@ -1,3 +1,10 @@
 library(data.table)
+library(compare)
 
 extlist <- data.frame(fread("/home/zhihao/Downloads/All_RDcall_18Feb15.csv"))
+
+curated <- data.frame(fread("/home/zhihao/Downloads/anno.rank.txt", header=TRUE))
+
+comparison <- compare(extlist[, c("Chr", "Start", "End", "Ref", "Alt")],
+                      curated[, c("Chr", "Start", "End", "Ref", "Alt")],
+                      allowAll=TRUE)
